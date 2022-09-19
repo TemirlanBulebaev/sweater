173fd8611af44f22464304877b56058c6d83b25d
Temirlan <tbulebaev@bk.ru>
2022-09-19 04:41:24 +0300

    add img and static

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -14,6 +14,8 @@ public class Message {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
+    
+    private String filename;
 
     public User getAuthor() {
         return author;
@@ -58,5 +60,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
